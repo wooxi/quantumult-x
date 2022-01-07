0daefc35884968f251d980b3c22c40639ac6287e
wooxi <59468499+wooxi@users.noreply.github.com>
2022-01-07 21:31:23 +0800

    Add files via upload

---
+++ pear.js
@@ -0,0 +1,50 @@
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/api/Movie/WatchMovie';
+
+const checkvip = '/api/Account/CheckVip';
+
+const vipinfo = '/api/account/IndexDetail';
+
+const wg = '/api/movie/WatchFgMovie';
+
+const mh = '/api/Cartoon/LookPhoto';
+
+const tp = '/api/PictureSet/LookPhoto';
+
+if (url.indexOf(vip) != -1) {
+	obj["canWath"] = "true";
+	body = JSON.stringify(obj);
+ }
+
+if (url.indexOf(checkvip) != -1) {
+	obj["data"] = "1";
+   obj["value"] = "true";
+	body = JSON.stringify(obj);
+ }
+
+if (url.indexOf(vipinfo) != -1) {
+	obj["nickName"] = "wooxi";
+   obj["vipLevel"] = "101";
+   obj["vipEndTime"] = "2099-12-31";
+   obj["cartoonVip"] = "true";
+	body = JSON.stringify(obj);
+ }
+
+if (url.indexOf(wg) != -1) {
+   obj["value"] = "true";
+	body = JSON.stringify(obj);
+ }
+
+if (url.indexOf(mh) != -1) {
+   obj["value"] = "true";
+	body = JSON.stringify(obj);
+ }
+
+if (url.indexOf(tp) != -1) {
+   obj["value"] = "true";
+	body = JSON.stringify(obj);
+}
+$done({body});
